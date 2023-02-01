503e60422fe217100c275ada6e4c49970711addd
MSNoorul <111111285+MSNoorul@users.noreply.github.com>
2023-02-01 05:36:41 -0500

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,13 +3,9 @@ import React from 'react'
 const About = () => {
   return (
     <section className="section about-section">
-    <h1 className="section-title">about us</h1>
+    <h1 className="section-title">about me</h1>
     <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
-      repudiandae architecto qui adipisci in officiis, aperiam sequi atque
-      perferendis eos, autem maiores nisi saepe quisquam hic odio consectetur
-      nobis veritatis quasi explicabo obcaecati doloremque? Placeat ratione
-      hic aspernatur error blanditiis?
+     JavaScript Developer with experience in HTML, CSS, Tailwind CSS, JavaScript, ReactJS, NodeJS, ExpressJS, and MongoDB. Recently completed a full stack JavaScript development course and seeking entry-level opportunities in the field as a fresher. Strong problem-solving skills and a passion for technology.
     </p>
   </section>
   )
